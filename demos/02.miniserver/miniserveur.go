70704c48f8af1d0049a69af4fffe14b8defd06a0
Rougetet Arnaud <arnaud.rougetet@etudiant.univ-lille1.fr>
2018-04-05 16:15:52 +0200

    Deux premier exo Arnaud

---
+++ demos/02.miniserver/miniserveur.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func printPage(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println(r.URL.Path)
+	file, err := os.Open("www" + r.URL.Path)
+	if err != nil {
+		io.WriteString(w, "Fichier non présent")
+	} else {
+		io.Copy(w, file)
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/", printPage)
+
+	http.ListenAndServe(":8080", nil)
+}
