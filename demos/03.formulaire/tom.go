8e62bdfd9b0708e687981396131b4d6d86cb8099
tomMoulard <tom.moulard@epita.fr>
2018-04-05 16:04:59 +0200

    simple from

---
+++ demos/03.formulaire/tom.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+type Hello struct{}
+
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("index.html")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Fprint(w, "Bonjour, ", r.Form["userName"][0], ".\n")
+		i, err := strconv.Atoi(r.Form["birthYear"][0])
+		i = 2018 - i
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Fprintln(w, "Vous avez", i, "ans.")
+		}
+	}
+
+}
+
+func main() {
+	var h Hello
+	http.ListenAndServe("localhost:8080", h)
+}
