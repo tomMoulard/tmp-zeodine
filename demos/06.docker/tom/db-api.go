f4ec7c208898c77434603c28bfb4f9f90d48edd2
tomMoulard <tom.moulard@epita.fr>
2018-04-10 12:23:15 +0200

    Mode struct Proper Preparation of query and resolving connection bug

--- demos/06.docker/tom/db-api.go
+++ demos/06.docker/tom/db-api.go
@@ -7,42 +7,25 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
 )
 
-var id int = 0
-var dataSource string
-var db *sql.DB
-var err error
-
-func init() {
-
-	fmt.Println("Initializing the database...")
-
-	dataSource = "tom:multipass@tcp(db:3306)/"
-
-	fmt.Printf("dataSource = %v\n", dataSource)
-
-	// Connecting to db
-	db, err = sql.Open("mysql", dataSource)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
+type DbManager struct {
+	id         int
+	dataSource string
+	db         *sql.DB
+	err        error
 }
 
-func showTable(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func (dbm DbManager) showTable(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	// Query the table
-	que, err := db.Query("SELECT * FROM zeodine.example")
+	que, err := dbm.db.Query("SELECT * FROM zeodine.example")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	fmt.Fprintln(w, "Table content:")
 	for que.Next() {
@@ -50,88 +33,132 @@ func showTable(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		var name string
 		err = que.Scan(&id, &name)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 		}
 		fmt.Fprintln(w, id, ":", name)
 	}
 }
 
-func add(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (dbm DbManager) add(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	// Inserting some stuff
-	i := strconv.Itoa(id)
-	ins, err := db.Query(
-		"INSERT INTO zeodine.example VALUES (" + i + ",'" + ps.ByName("name") + "')")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer ins.Close()
+	i := strconv.Itoa(dbm.id)
 
-	fmt.Fprintln(w, "You've just insered", ps.ByName("name"), "with the id of", id)
-	id += 1
+	// Prepare
+	quer, err := dbm.db.Prepare("INSERT INTO zeodine.example VALUES (?, ?)")
+	if err != nil {
+		log.Println(err)
+	}
+	defer quer.Close()
+
+	// Query
+	_, err = quer.Exec(i, ps.ByName("name"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Fprintln(w, "You've just insered", ps.ByName("name"), "with the id of", dbm.id)
+	dbm.id += 1
 }
 
-func rm(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (dbm DbManager) rm(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	// Deleting some stuff
-	ins, err := db.Query(
-		"DELETE FROM zeodine.example WHERE id = " + ps.ByName("id"))
+	quer, err := dbm.db.Prepare("DELETE FROM zeodine.example WHERE id = ?")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	defer ins.Close()
+	defer quer.Close()
+
+	_, err = quer.Exec(ps.ByName("id"))
+	if err != nil {
+		log.Println(err)
+	}
 
 	fmt.Fprintln(w, "You've just deleted the name corresponding of the id:", ps.ByName("id"))
 
 }
 
-func quit(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func (dbm DbManager) quit(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	// Dropping database
-	_, err := db.Exec("DROP DATABASE zeodine")
+	_, err := dbm.db.Exec("DROP DATABASE zeodine")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Database DROPED")
+	log.Println("Database DROPED")
 	os.Exit(1) // Shutdown api
 }
 
-func setupDB() {
-
-	// Creating a new database
-	_, err := db.Exec("CREATE DATABASE zeodine")
+func (dbm DbManager) pingDB() bool {
+	time.Sleep(time.Second * 5)
+	err := dbm.db.Ping()
 	if err != nil {
-		// fmt.Println(err)
-		log.Printf("A la création de la database zeodine, j'ai obtenu l'erreur : %v\n", err)
+		log.Println(err.Error())
+		return false
 	}
-	fmt.Println("Database created")
+	return true
+}
+
+func (dbm DbManager) setupDB() {
+	log.Println("Initializing the database...")
+
+	dbm.dataSource = "tom:multipass@tcp(db:3306)/"
+
+	log.Printf("dataSource = %v\n", dbm.dataSource)
+
+	// Connecting to db
+	dbm.db, dbm.err = sql.Open("mysql", dbm.dataSource)
+	if dbm.err != nil {
+		panic(dbm.err.Error())
+	}
+	defer dbm.db.Close()
+
+	var pinged bool = false
+	for i := 0; i < 3 && !pinged; i++ {
+		log.Println("Connecting to database ... try:", i)
+		if dbm.pingDB() {
+			pinged = true
+		}
+	}
+	if !pinged {
+		log.Println("Could not connect to db in time")
+		os.Exit(0)
+	}
+	log.Println("Connected")
+	// Creating a new database
+	_, dbm.err = dbm.db.Exec("CREATE DATABASE IF NOT EXISTS zeodine")
+	if dbm.err != nil {
+		// log.Println(err)
+		log.Printf("Error when creating db: %v\n", dbm.err)
+	}
+	log.Println("Database created")
 
 	// Using the fleshly created database:
-	_, err = db.Exec("USE zeodine")
-	if err != nil {
-		panic(err)
+	_, dbm.err = dbm.db.Exec("USE zeodine")
+	if dbm.err != nil {
+		log.Println(dbm.err)
 	}
 
 	// Creating a new table to insert some elements
-	_, err = db.Exec("CREATE TABLE zeodine.example ( id integer, data varchar(32) )")
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
+	_, dbm.err = dbm.db.Exec("CREATE TABLE IF NOT EXISTS zeodine.example ( id integer, data varchar(32) )")
+	if dbm.err != nil {
+		log.Println("Error when creating table:", dbm.err)
 	}
-	fmt.Println("Table created")
+	log.Println("Table ready to be used")
 }
 
 func main() {
-	defer db.Close()
+	var dbm DbManager
 
-	setupDB()
+	dbm.setupDB()
 
 	// Routing
 	router := httprouter.New()
-	router.GET("/", showTable)
-	router.GET("/add/:name", add)
-	router.GET("/rm/:id", rm)
-	router.GET("/quit", quit)
+	router.GET("/", dbm.showTable)
+	router.GET("/add/:name", dbm.add)
+	router.GET("/rm/:id", dbm.rm)
+	router.GET("/quit", dbm.quit)
 
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
