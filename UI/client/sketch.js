cb0ab86e2c9bf876745add66086e862b3726dc72
tomMoulard <tom.moulard@epita.fr>
2018-04-12 18:22:19 +0200

    adding the keypressed event

--- UI/client/sketch.js
+++ UI/client/sketch.js
@@ -6,47 +6,44 @@ var nbCards;
 var nbCardsMax;
 var cardPos;
 var timeToSave;
+var bottomCards;
 
 function setup() {
-    h = windowHeight;
-    w = windowWidth;
+    h                  = windowHeight;
+    w                  = windowWidth;
     createCanvas(w, h);
-    cards = [];
-    loadedCards = [];
+    cards              = []; //Personal cards
+    loadedCards        = []; //all cards provided
+    bottomCards        = []; //cards at the bottom of the page
     mouseIsPressedHere = false;
-    nbCards = floor(w / 100);
-    nbCardsMax = 5;
-    cardPos = 0;
-    timeToSave = 0;
+    nbCards            = floor(w / 100); // max card in a row
+    nbCardsMax         = 5; //total cards provided
+    // TODO: get this from the api
+    cardPos            = 0; //Bottom first card
+    timeToSave         = 0; //timer to save cards
 
     for (var i = 0; i < nbCardsMax; i++) {
         var url = "http://147.135.194.248/cards/" + i;
         // var cardJson = loadJSON(url, createCard);
         // append(loadedCards, -1);
-        var tmpCard = new Card(0, h - 155, "maxresdefault.jpg", "" + i);
+        var tmpCard = new Card(-100, -100, "maxresdefault.jpg", "" + i);
         append(loadedCards, tmpCard);
     }
-    console.log(loadedCards)
+    buildBottomCards()
+    console.log(loadedCards, bottomCards)
 }
 
 function draw() {
     background(51);
     // Showing all cards
     for (var i = 0; i < cards.length; i++) {
-        if (cards[i]) {
-            cards[i].display();
-        }
+        cards[i].display();
+    }
+    for (var i = 0; i < bottomCards.length; i++) {
+        bottomCards[i].display();
     }
     // showing preloaded cards starting with cardPos
-    var cardPosTmp = cardPos;
-    for (var i = cardPos; i < cardPos + nbCards; i++) {
-        if (cardPosTmp >= nbCardsMax) { cardPosTmp = 0; }
-        var posX = map(i, 0, nbCards, 80, w-20)
-        // var tmpCard = new Card(posX, h - 155, "maxresdefault.jpg", "CardID:" + cardPosTmp)
-        loadedCards[cardPosTmp].x = posX;
-        loadedCards[cardPosTmp].display();
-        cardPosTmp += 1;
-    }
+    // first build a "smaller" list
     if (mouseIsPressedHere) {
         cards[thisCardsID].x = mouseX;
         cards[thisCardsID].y = mouseY;
@@ -67,16 +64,22 @@ function mousePressed() {
     if (mouseX < w && mouseY < h - 245) {
         cardID = isCard(mouseX, mouseY, cards);
         if (cardID == -1) {
-            // var newCard = new Card(mouseX, mouseY, "maxresdefault.jpg", "CardID:" + cards.length);
+            // var newCard = new Card(mouseX,
+            //                        mouseY,
+            //                        "maxresdefault.jpg",
+            //                        "CardID:" + cards.length);
             // append(cards, newCard);
         } else {
             mouseIsPressedHere = true;
-            thisCardsID = cardID;
+            thisCardsID        = cardID;
         }
-    } else if (mouseX < w && mouseY < h){
-        cardID = isCard(mouseX, mouseY, loadedCards);
-        console.log(cardID, loadedCards[cardID])
-        var tmpCard = new Card(w/2, h/2, loadedCards[cardID].imgURL, loadedCards[cardID].desc)
+    } else if (mouseX < w && mouseY < h) {
+        cardID = isCard(mouseX, mouseY, bottomCards);
+        console.log(cardID, bottomCards[cardID])
+        var tmpCard = new Card(w / 2,
+                               h / 2,
+                               bottomCards[cardID].imgURL,
+                               bottomCards[cardID].desc)
         console.log(cardID, tmpCard);
         append(cards, tmpCard);
     }
@@ -86,7 +89,20 @@ function mouseReleased() {
     mouseIsPressedHere = false;
 }
 
+function keyPressed() {
+    if (keyCode == LEFT_ARROW) {
+        cardPos += 1
+        if (cardPos >= nbCardsMax) { cardPos = 0; }
+    } else if (keyCode == RIGHT_ARROW) {
+        cardPos -= 1
+        if (cardPos < 0) { cardPos = nbCardsMax - 1; }
+    }
+    buildBottomCards();
+}
+
 function isCard(x, y, list) {
+    console.log("Checking if (" + x + "," + y + ") is inside a card")
+    console.log(list)
     for (var i = 0; i < list.length; i++) {
         if (x > list[i].x - list[i].w &&
             x < list[i].x + list[i].w &&
@@ -100,6 +116,22 @@ function isCard(x, y, list) {
 function createCard(json) {
     console.log(json);
     var posX = map(json.id, 0, w, 0, nbCards);
-    var newCard = new Card(posX, h - 185, json.img, json.text);
+    var newCard = new Card(posX,
+                           h - 185,
+                           json.img,
+                           json.text);
     append(cards, tmpCard);
+}
+
+function buildBottomCards() {
+    for (var i = 0; i < nbCards; i++) {
+        if (cardPos >= nbCardsMax) { cardPos = 0; }
+        var posX = map(i, 0, nbCards, 80, w + 20);
+        var tmpCard = new Card(posX,
+                               h - 155,
+                               loadedCards[cardPos].imgURL,
+                               loadedCards[cardPos].desc);
+        append(bottomCards, tmpCard);
+        cardPos += 1;
+    }
 }
