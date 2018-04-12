d0639aa54f9b6586ccf93c55b53bb253e901359e
tomMoulard <tom.moulard@epita.fr>
2018-04-12 11:25:10 +0200

    simple card stuff

--- UI/tom/sketch.js
+++ UI/tom/sketch.js
@@ -1,36 +1,22 @@
-var angle;
-var slider;
-var h, w;
-var newLen;
+var cards;
 function setup() {
-    h      = windowHeight - 50;
+    h      = windowHeight;
     w      = windowWidth;
-    newLen = 0.6667;
+    cards  = [];
     createCanvas(w, h);
-    slider = createSlider(0, TWO_PI, PI/4, 0.001);
 }
 
 function draw() {
     background(51);
-    angle = slider.value();
-    stroke(255);
-    translate(w / 2, height);
-    branch(h / 4);
-}
-
-function branch(len){
-    strokeWeight(len * 0.25);
-    line(0, 0, 0, -len);
-    translate(0, -len);
-    if(len > 2){    
-        newbranch(len, angle);
-        newbranch(len, -angle);
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].display()
     }
 }
 
-function newbranch(len, angle){
-    push();
-    rotate(angle);
-    branch(len*newLen);
-    pop();
+function mousePressed() {
+    if (mouseX < w && mouseY < h) {
+        console.log(mouseX, mouseY)
+        var newCard = new Card(mouseX, mouseY)
+        append(cards, newCard)
+    }
 }
